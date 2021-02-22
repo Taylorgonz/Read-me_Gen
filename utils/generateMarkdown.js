8de3030da1277c4e621bfb7714e71c8f7ce0938d
Taylor Gonzales <hello@taylorgonz.com>
2021-02-21 20:06:03 -0500

    git ignore and markdown generator finished

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,67 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+
+  if (data.license == "MIT") {
+    data.license = "[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)"
+  } else if (data.license == "APACHE 2.0") {
+    data.license = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  } else if (data.license == "GPL 3.0") {
+    data.license = "[![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html)"
+  } else if (data.license == "BSD 3") {
+    data.license = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+  } else {
+    data.license = ""
+  }
+
+
   return `# ${data.title}
+  
+${data.license}
+
+## Description 
+${data.description}
+
+
+## Table of Contents (Optional)
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Contact](#contact-me)
+
+
+## Installation
+In order to install the necessary dependencies, open the console and run the following.
+${data.installation}
+
+## Usage 
+${data.usage}
+
+
+## License
+This project is licensed under :
+\n
+${data.license}
+\n
+Click on the badge for further information.
+
+
+## Contributing
+${data.contribute}
+
+
+## Tests
+In order to test open the console and run the following:
+
+${data.tests}
+
+## Contact Me
+ If you have any further questions message me on [GitHub](https://github.com/${data.github}),
+ or send me an email to \n
+ ${data.name}: ${data.email}.
 
 `;
 }
